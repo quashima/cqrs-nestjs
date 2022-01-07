684813cfea9c42dfc1d70d1968997fc76ad039f0
quashima <ex.rwksm1024@gmail.com>
2022-01-07 21:54:50 +0900

    connect db

--- src/modules/user/id/app/get-user-by-id.handler.ts
+++ src/modules/user/id/app/get-user-by-id.handler.ts
@@ -1,11 +1,18 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../../../shared/infrastructure/entities/user.entity';
+import { Repository } from 'typeorm';
 import { GetUserByIdQuery } from './get-user-by-id.query';
 
 @QueryHandler(GetUserByIdQuery)
 export class GetUserByIdHandler implements IQueryHandler<GetUserByIdQuery> {
-
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+      ) {}
+    
   async execute(query: GetUserByIdQuery) {
     const { id } = query;
-    return `by ${id}`;
+    return this.userRepository.find({ where: { id: id } });
   }
 }
