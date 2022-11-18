f4975875ff0c6526433ed7a6d06a5e59a8516a91
quashima <ex.rwksm1024@gmail.com>
2022-11-18 19:26:07 +0900

    add log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const localhost = 3000;
+  await app.listen(localhost);
+  console.log('start localhost', localhost);
 }
 bootstrap();
